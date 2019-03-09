fe007f49243b60a6abf24700aedb9b761c600b89
Josue Alvarez <josuetec2003@gmail.com>
2019-03-09 17:15:42 -0600

    mysql connection settings changed

--- prados_uni/settings.py
+++ prados_uni/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'prados_uni.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'connection.cnf'),
+        },
     }
 }
 
